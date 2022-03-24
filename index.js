ee19da6a7799cdde6a033be2751404f5f94c2263
Chupexy <osaretin520@gmail.com>
2022-03-24 14:56:06 +0100

    updated the cases

--- index.js
+++ index.js
@@ -44,4 +44,4 @@ const server= http.createServer((req,res) =>{
 });
 const PORT =process.env.PORT || 5000;
 server.listen(PORT,  ()=>
-    console.log('server running at local address'));
+    console.log(`server running at port ${PORT}`));
